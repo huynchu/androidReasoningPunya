a539b0d2a221668789c6a3aed376ceeafb6bad29
Megan Goulet <megan.elizabeth.goulet@gmail.com>
2023-04-17 18:00:21 -0400

    Create Explainer.java
    wip

---
+++ AndroidRules/app/src/main/java/wvw/mobile/rules/explanation/Explainer.java
@@ -0,0 +1,19 @@
+package wvw.mobile.rules.explanation;
+
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Model;
+
+import wvw.mobile.rules.eyebrow.Reasoner;
+
+/**
+ * Provides user-friendly explanations
+ */
+public abstract class Explainer {
+
+    private Model baseModel;
+    private Reasoner reasoner;
+
+    public Explainer(){}
+
+    public abstract String fullExplanation();
+}
