81f5aefdacb33cc7744956d0493fdafec99d8cde
Megan Goulet <megan.elizabeth.goulet@gmail.com>
2023-04-18 13:39:06 -0400

    Update Explainer.java

--- AndroidRules/app/src/main/java/wvw/mobile/rules/explanation/Explainer.java
+++ AndroidRules/app/src/main/java/wvw/mobile/rules/explanation/Explainer.java
@@ -6,17 +6,62 @@ import com.hp.hpl.jena.rdf.model.Model;
 import wvw.mobile.rules.eyebrow.Reasoner;
 
 /**
- * Provides user-friendly explanations
+ * The <code>Explainer</code> component produces user-friendly
+ * explanations of recommendations derived from the <code>Reasoner</code>
  */
-public abstract class Explainer {
+/*
+@DesignerComponent(version = PunyaVersion.EXPLAINER_COMPONENT_VERSION,
+    nonVisible = true,
+    category = ComponentCategory.LINKEDDATA,
+)
+@SimpleObject
+ */
+public class Explainer {
 
     private Model baseModel;
     private Reasoner reasoner;
+    private String rules;
 
+    /**
+     * Creates a new Explainer component.
+     */
     public Explainer(){}
 
-    // TODO: Change this to be accomodating for multiple different types
-    //       of explanations.
-    public abstract String GetFullExplanation();
+    /// region Properties
+
+    public Model Model(){
+        return this.baseModel;
+    }
+
+    public void Model(Model model){
+        this.baseModel = model;
+    }
+
+    public Reasoner Reasoner(){
+        return this.reasoner;
+    }
+
+    public void Reasoner(Reasoner reasoner){
+        this.reasoner = reasoner;
+    }
+
+    public String Rules(){
+        return this.rules;
+    }
+
+    public void Rules(String rules){
+        this.rules = rules;
+    }
+
+    ///endregion
+    ///region Methods
+
+    public String GetFullContrastiveExplanation(){
+        return "Not Implemented Yet";
+    }
+
+    public String GetFullContextualExplanation() {
+        return "Not Implemented Yet";
+    }
 
 }
